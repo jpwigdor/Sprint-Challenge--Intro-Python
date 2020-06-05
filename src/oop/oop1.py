63a0cd3e217c584caa79003d17ab8e310c90b672
James Wigdor <jpwigdor@gmail.com>
2020-06-05 11:47:11 -0400

    complete oop1 file

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Vehicle is the base class - everything else builds off of this one.
+class Vehicle:
+    def __init__(self):
+        """ I believe __init__ is auto-added under the hood if you dont define this. But I added it for a more complete understanding."""
+        pass
+
+# starting on the ground vehicle path
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
+
+# starting on the air vehicle path
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
